308202bb359b7133514e32847650b878178fccf8
Hironao OTSUBO <motemen@gmail.com>
2022-10-27 17:47:34 +0900

    devcontainer

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -e
+
 assert() {
   expected="$1"
   input="$2"
 
   ./9cv "$input" > tmp.s
-  riscv64-unknown-elf-gcc -o tmp tmp.s
-  spike "$(brew --prefix riscv-pk)/riscv64-unknown-elf/bin/pk" ./tmp
+  riscv64-$RISCV_HOST-gcc -static -o tmp tmp.s
+
+  set +e
+  spike "$RISCV/riscv64-$RISCV_HOST/bin/pk" ./tmp
   actual="$?"
+  set -e
 
   if [ "$actual" = "$expected" ]; then
     echo "$input => $actual"
@@ -19,5 +24,6 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 21 "5+20-4"
 
 echo OK
