1e8f1383599930891f38a27dd38e56beb242d4d4
Hironao OTSUBO <motemen@gmail.com>
2022-10-27 18:11:49 +0900

    tokenize

--- 9cv.c
+++ 9cv.c
@@ -1,5 +1,118 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+Token *token;
+
+char *user_input;
+
+void verror_at(char *loc, char *fmt, va_list ap) {
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  verror_at(loc, fmt, ap);
+}
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(token->str, fmt, ap);
+}
+
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    return false;
+  }
+
+  token = token->next;
+  return true;
+}
+
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    error("not '%c'", op);
+  }
+
+  token = token->next;
+}
+
+int expect_number() {
+  if (token->kind != TK_NUM) {
+    error("not a number");
+  }
+
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("cannot tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,24 +120,21 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  user_input = argv[1];
+  token = tokenize(user_input);
 
   printf(".global main\n");
   printf("main:\n");
-  printf("  li t0, %ld\n", strtol(p, &p, 10));
+  printf("  li t0, %d\n", expect_number());
 
-  while (*p) {
-    if (*p == '+') {
-      printf("  addi t0, t0, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    if (*p == '-') {
-      printf("  addi t0, t0, %ld\n", strtol(p, &p, 10));
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  addi t0, t0, %d\n", expect_number());
       continue;
     }
 
-    fprintf(stderr, "Bad char: '%c'\n", *p);
-    return 1;
+    expect('-');
+    printf("  addi t0, t0, -%d\n", expect_number());
   }
 
   printf("  mv a0, t0\n");
