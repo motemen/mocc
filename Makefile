79ae9f99fcf2f9623e7d1e6d8141e630c4b57fd0
Hironao OTSUBO <motemen@gmail.com>
2022-11-04 19:07:08 +0900

    テストを test/test.c にだいぶ移植

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ OBJS=$(SRCS:.c=.o)
 $(OBJS): 9cv.h
 
 test: 9cv
+	./9cv test/test.c > tmp.s && riscv64-unknown-elf-gcc tmp.s -o test.out && spike "$(RISCV)/riscv64-$(RISCV_HOST)/bin/pk" ./test.out
 	prove -v ./test.sh
 
 clean:
---
+++ test/test.c
@@ -0,0 +1,236 @@
+/*
+ * 9cv でコンパイルして実行されるテスト
+ */
+
+int test_count;
+int fail_count;
+
+int is(int expected, int actual, char *message) {
+  if (ok(actual == expected, message) == 0) {
+    printf("# expected %d but got %d\n", expected, actual);
+  }
+}
+
+int ok(int ok, char *message) {
+  test_count = test_count + 1;
+  if (ok) {
+    printf("ok - %s\n", message);
+    return 1;
+  } else {
+    printf("not ok - %s\n", message);
+    fail_count = fail_count + 1;
+    return 0;
+  }
+}
+
+int main() {
+  test_count = 0;
+  fail_count = 0;
+
+  test_arithmetic();
+  test_func();
+  test_array();
+  test_string_literal();
+  test_sizeof();
+  test_for_while();
+  test_pointer();
+
+  printf("1..%d\n", test_count);
+
+  return fail_count;
+}
+
+int test_arithmetic() {
+  printf("# arithmetic\n");
+  is(1, 1, "1");
+  is(36, 4 * 9, "4 * 9");
+  is(-3, 1 - 6 * 2 / 3, "1 - 6 * 2 / 3");
+  is(0, 0, "0");
+  is(42, 42, "42");
+  is(21, 5 + 20 - 4, "5+20-4");
+  is(41, 12 + 34 - 5, " 12 + 34 - 5 ");
+  is(77, (77), "(77)");
+  is(6, 1 + (2 + 3), "1+(2+3)");
+  is(0, 3 - (2 + 1), "3-(2+1)");
+  is(12, 3 * 4, "3*4");
+  is(33, (3 + 8) * 3, "(3+8)*3");
+  is(47, 5 + 6 * 7, "5+6*7");
+  is(15, 5 * (9 - 6), "5*(9-6)");
+  is(4, (3 + 5) / 2, "(3+5)/2");
+  is(5, (-3 * +5) + 20, "(-3*+5)+20");
+  is(1, 1 < (11 * 9), "1<(11*9)");
+  is(0, 13 < 13, "13<13");
+  is(1, (1 + 1) > 1, "(1+1)>1");
+  is(0, 13 > 13, "13 > 13");
+  is(1, 13 >= 13, "13 >= 13");
+  is(1, 13 <= 13, "13 <= 13");
+  is(0, 42 >= 43, "42 >= 43");
+  is(1, 123 == 123, "123 == 123");
+  is(0, 123 == 321, "123 == 321");
+  is(0, 123 != 123, "123 != 123");
+  is(1, 123 != 321, "123 != 321");
+
+  is(0, 65536 * 65536, "65536 * 65536 overflows");
+}
+
+int test_func() {
+  printf("# func\n");
+  is(1, fact(0), "fact(0)");
+  is(120, fact(5), "fact(5)");
+  is(3628800, fact(10), "fact(10)");
+
+  is(1, fib(1), "fib(1)");
+  is(2, fib(2), "fib(2)");
+  is(3, fib(3), "fib(3)");
+  is(89, fib(10), "fib(10)");
+
+  is(66666, add(33333, 33333), "add(33333, 33333)");
+  is(-66666, add(33333, -99999), "add(33333, -99999)");
+
+  is(50000, f_if(1, 50000, 60000), "f_if(1, 50000, 60000)");
+  is(60000, f_if(0, 50000, 60000), "f_if(1, 50000, 60000)");
+
+  is(2, f_return_return(1), "f_return_return(1)");
+
+  is(13, f_comment(), "f_comment()");
+}
+
+int fact(int n) {
+  if (n == 1 || n == 0)
+    return 1;
+  return n * fact(n - 1);
+}
+
+int fib(int n) {
+  if (n == 1 || n == 0)
+    return 1;
+  return fib(n - 2) + fib(n - 1);
+}
+
+int add(int x, int y) { return x + y; }
+
+int f_if(int b, int x, int y) {
+  if (b) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+int f_return_return(int val) {
+  return val * 2;
+  return val;
+}
+
+int f_comment() {
+  return 1 + /* 2 + */ 3 // ;
+                 * 4;
+}
+
+int test_array() {
+  printf("# array\n");
+
+  int a[100];
+
+  a[49] = 99;
+  a[50] = 999;
+  a[51] = 9999;
+  is(999, a[50], "a[50] = 999; a[50]");
+  is(999, *(a + 50), "a[50] = 999; *(a+50)");
+
+  *a = 12345;
+  is(12345, a[0], "*a = 12345; a[0]");
+  is(12345, *a, "*a = 12345; *a");
+
+  int mat[11][13];
+  mat[9][11] = 4567;
+  is(4567, mat[9][11], "mat[9][11] = 4567; mat[9][11]");
+  is(4567, *(mat[9] + 11), "mat[9][11] = 4567; *(mat[9]+11)");
+  is(4567, *(*(mat + 9) + 11), "mat[9][11] = 4567; *(*(mat+9)+11)");
+
+  *(*(mat + 5) + 7) = 8901;
+  is(8901, mat[5][7], "*(*(mat+5)+7) = 8901; mat[5][7]");
+  is(8901, *(mat[5] + 7), "*(*(mat+5)+7) = 8901; *(mat[5]+7)");
+  is(8901, *(*(mat + 5) + 7), "*(*(mat+5)+7) = 8901; *(*(mat+5)+7)");
+
+  (*(mat + 2))[4] = 1111;
+  is(1111, mat[2][4], "(*(mat+2)[4]) = 1111; mat[2][4]");
+
+  *(mat[8] + 12) = 9999;
+  is(9999, mat[8][12], "*(mat[8]+12) = 9999; mat[8][12]");
+}
+
+int test_string_literal() {
+  printf("# string literal\n");
+
+  char *s;
+  s = "Hello, world!";
+
+  is(s[0], 72, "s[0] == 72");
+  is(s[13], 0, "s[13] == 0");
+}
+
+int test_sizeof() {
+  printf("# sizeof\n");
+
+  int x;
+  char c;
+  int a[10];
+  int *p;
+  int mat[10][10];
+
+  is(4, sizeof(x), "sizeof(x)");
+  is(1, sizeof(c), "sizeof(c)");
+  is(40, sizeof(a), "sizeof(a)");
+  is(8, sizeof(p), "sizeof(p)");
+  is(400, sizeof(mat), "sizeof(mat)");
+  is(40, sizeof(mat[0]), "sizeof(mat[0])");
+  is(8, sizeof(&mat), "sizeof(mat&)");
+}
+
+int test_for_while() {
+  printf("# for while\n");
+
+  int sum;
+  sum = 0;
+  int n;
+  for (n = 1; n <= 10; n = n + 1) {
+    sum = sum + n;
+  }
+  is(55, sum, "for 1+..+10");
+
+  int x;
+  x = 1;
+  for (; x < 1000;)
+    x = x * 2;
+  is(1024, x, "for(;x<1000;) x=x*2;");
+
+  sum = 0;
+  n = 1;
+  while (n <= 10) {
+    sum = sum + n;
+    n = n + 1;
+  }
+  is(55, sum, "while 1+..+10");
+}
+
+int test_pointer() {
+  int x;
+  int *p;
+  int **pp;
+
+  p = &x;
+  pp = &p;
+
+  *p = 5656;
+  is(5656, x, "*p = 5656; x");
+
+  **pp = 8989;
+  is(8989, x, "**pp = 8989; x");
+
+  x = 7777;
+  is(7777, *p, "x = 7777; *p");
+  is(7777, **pp, "x = 7777; **pp");
+
+  ok(&x > 0, "&x > 0");
+}
